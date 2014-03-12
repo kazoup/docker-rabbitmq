bbf350225a7d69a128f92d19222433764098cd00
John Smith <info@whywouldwe.com>
2014-03-12 15:31:24 +0000

    add exec to ensure runit management commands work

--- run-rabbitmq.sh
+++ run-rabbitmq.sh
@@ -5,4 +5,4 @@ export HOME=/srv/kazoup/kazoup-services/rabbitmq/tmp
 trap '{ echo Stopping rabbitmq; /srv/kazoup/kazoup-services/rabbitmq/rabbitmq_server/sbin/rabbitmqctl stop; exit 0;}' EXIT
 
 echo 'Starting rabbitmq'
-/srv/kazoup/kazoup-services/rabbitmq/rabbitmq_server/sbin/rabbitmq-server
+exec /srv/kazoup/kazoup-services/rabbitmq/rabbitmq_server/sbin/rabbitmq-server
